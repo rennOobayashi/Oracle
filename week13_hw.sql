9aef5ba803921127ab0b05f4d5d6e6ebad7ec8d8
inhee <devforplayer@gmail.com>
2025-11-27 19:37:13 +0900

    v9.1 week hw

--- week13_hw.sql
+++ week13_hw.sql
@@ -7,3 +7,16 @@ begin
 end p_sub_no_update;
 
 execute p_sub_no_update('501', '201')
+
+create or replace procedure p_trade_delete (v_t_seq in trade.t_seq%type)
+is
+begin
+delete
+from trade
+where v_t_seq = t_seq;
+
+exception
+if no_data_found then
+dbms_output.put_line('no');
+end if;
+end p_trade_delete;
